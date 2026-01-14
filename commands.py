801a1aa609f1dc32b0c9cb3549c7c696a4d4296d
Raspberry Pi <jarmedilla16@gmail.com>
2026-01-14 14:07:38 -0600

    added good boy command

--- commands.py
+++ commands.py
@@ -4,6 +4,7 @@ import readchar
 from picarx.stt import Vosk
 
 SPIN_TIME = 5.75
+TILT_ANGLE = -5
 
 def main():
 	try:
@@ -12,11 +13,13 @@ def main():
 		
 		while True:
 			# key = readchar.readkey()
-			# key = key.lower()             
+			# key = key.lower()   
+			tilt_angle = 0          
 			
 			res = stt.listen(stream=False)
 			text = res.get("text", "") if isinstance(res, dict) else str(res)
 			text = text.lower().strip()
+			words = text.split(" ")
 			if not text:
 				continue
 			
@@ -24,14 +27,22 @@ def main():
 
 			
 			#if key == 'p':
-			if 'spin' in text:
+			if 'spin' in words:
 				px.set_dir_servo_angle(50)
-				px.forward(80)
+				px.forward(85)
 				time.sleep(SPIN_TIME)
 				px.forward(0)
 				px.set_dir_servo_angle(0)
-			
-	
+				
+			elif 'good' in words:
+				good_position = text.index('good')
+				if good_position+1 < len(words) and words[good_position+1] == 'boy':
+					for i in range(5):
+						px.set_cam_tilt_angle(TILT_ANGLE)
+						time.sleep(0.3)
+						px.set_cam_tilt_angle(-1*TILT_ANGLE)
+						time.sleep(0.3)
+				px.set_cam_tilt_angle(0)
 	finally:
 		px.set_cam_tilt_angle(0)
 		px.set_cam_pan_angle(0)  
